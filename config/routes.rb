8e24cba04fdb083c886801ef2571beb4465f1158
Melissa Mahan <2melissamahan@gmail.com>
2014-11-15 06:21:56 -0500

    Now running on Rangular!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'application#index'
+  get '*path' => 'application#index'
+
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
